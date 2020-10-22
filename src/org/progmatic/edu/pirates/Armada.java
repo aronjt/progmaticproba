743f03702c82e070edc8ac79afa79544d247037c
aronjt <jtelekiaron@gmail.com>
2020-10-22 14:56:27 +0200

    i putted in the gitignore

---
+++ src/org/progmatic/edu/pirates/Armada.java
@@ -0,0 +1,16 @@
+package org.progmatic.edu.pirates;
+
+
+import java.util.ArrayList;
+
+public class Armada {
+    private ArrayList<Ship> armada = new ArrayList<Ship>();
+
+    public void fillArmada() {
+        int size = (int) (Math.random()*10+1);
+        for (int i = 0; i < size; i++) {
+            armada.add(new Ship());
+        }
+    }
+
+}
--- src/org/progmatic/edu/pirates/Ship.java
+++ src/org/progmatic/edu/pirates/Ship.java
@@ -3,11 +3,11 @@ package org.progmatic.edu.pirates;
 import java.util.ArrayList;
 
 public class Ship {
-    private ArrayList<Pirate> pirates = new ArrayList<>();
+    private ArrayList<Pirate> pirates = new ArrayList<Pirate>();
     private Pirate captain;
 
     public void fillShip() {
-        int size = (int) (Math.random()*100+1);
+        int size = (int) (Math.random()*10+1);
         for (int i = 0; i < size; i++) {
             pirates.add(new Pirate());
         }
@@ -26,11 +26,11 @@ public class Ship {
         int shipScore = pirates.size() + captain.getAlcoholLevel();
         int otherShipScore = otherShip.pirates.size() + otherShip.captain.getAlcoholLevel();
         if (shipScore > otherShipScore) {
-            int deaths = (int) (Math.random()*otherShip.pirates.size());
+            int deaths = (int) (Math.random()*otherShip.pirates.size()) + 1;
             if (deaths >= otherShip.pirates.size()) {
                 otherShip.pirates.clear();
             } else {
-                for (int i = 0; i < deaths-1; i++) {
+                for (int i = 0; i < deaths; i++) {
                     otherShip.pirates.remove(i);
                 }
             }
@@ -42,11 +42,11 @@ public class Ship {
             }
             return true;
         } else {
-            int deaths = (int) (Math.random()*pirates.size());
+            int deaths = (int) (Math.random()*pirates.size()) + 1;
             if (deaths >= pirates.size()) {
                 pirates.clear();
             } else {
-                for (int i = 0; i < deaths-1; i++) {
+                for (int i = 0; i < deaths; i++) {
                     pirates.remove(i);
                 }
             }
